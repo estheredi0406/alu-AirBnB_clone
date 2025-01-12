4358d3e14fdde168d3761425eefd3102a99012ab
Avellin003 <uavellinbonaparte@gmail.com>
2025-01-12 12:35:58 +0200

    merging

--- models/base_model.py
+++ models/base_model.py
@@ -5,19 +5,30 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialization of the BaseModel class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation of the BaseModel class"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Update the updated_at attribute with the current datetime and save to storage"""
+        """
+        Update the updated_at
+        attribute with the current datetime and save to storage
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):

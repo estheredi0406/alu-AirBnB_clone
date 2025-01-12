277125d3b585aa991a95238bddd184b207d5de2f
estheredi0406 <e.sessenou@alustudent.com>
2025-01-12 12:16:32 +0200

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -5,25 +5,36 @@
 import uuid
 from datetime import datetime
 
-class BaseModel:
-    def __init__(self):
-        """Initialization of the BaseModel class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
-    def __str__(self):
+class BaseModel:
+    def _init_(self, *args, **kwargs):
+        """Initialization of the BaseModel class"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "_class_":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def _str_(self):
         """String representation of the BaseModel class"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self._class.name}] ({self.id}) {self.dict_}"
 
     def save(self):
-        """Update the updated_at attribute with the current datetime and save to storage"""
+        """
+        Update the updated_at
+        attribute with the current datetime and save to storage
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns the dict representation of BaseModel class"""
-        return {
-            "id": self.id,
-            "created_at": self.created_at.isoformat(),
-            "updated_at": self.updated_at.isoformat()
-        }
+        dictionary = self._dict_.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary

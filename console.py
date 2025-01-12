91cc979ba2c8e48fd260497594bf20379b63730b
Avellin003 <uavellinbonaparte@gmail.com>
2025-01-12 17:35:54 +0200

    console

--- console.py
+++ console.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
-
-'''
-Creation of the Console 001
-
-'''
-
-
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    '''Console class'''
-    prompt='(hbnb) '
+    """Command interpreter for HBNB"""
+    prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        '''Quit command to exit the program'''
+        """Quit command to exit the program"""
         return True
 
-    def do_help (self, arg):
-        '''Help command to show the help'''
-        return super().do_help(arg)
-
-    def do_EOF(self):
-        '''EOF command to exit the program'''
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print()
         return True
 
     def emptyline(self):
+        """Do nothing on empty input line"""
         pass
 
-    
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()

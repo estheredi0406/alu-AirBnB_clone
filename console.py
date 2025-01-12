3791d268e426afcdd926379bf32fb808e761749e
Avellin003 <uavellinbonaparte@gmail.com>
2025-01-12 16:47:29 +0200

    console.py

--- console.py
+++ console.py
@@ -11,28 +11,28 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     '''
-    
+    Console class
     '''
-    prompt='(hbnb)'
+    prompt='(hbnb) '
 
     def do_quit(self, arg):
-        '''
-        
-        '''
+        '''Quit command to exit the program'''
         return True
 
     def do_help (self, arg):
         '''
-        
+        Help command to show the help
         '''
-        print('Quit command to exit the program')
+        return super().do_help(arg)
 
-    def do_EOF(self, arg):
+    def do_EOF(self):
         '''
-        
+        EOF command to exit the program
         '''
-        print()
         return True
+
+    def emptyline(self):
+        pass
 
     
 if __name__ == '__main__':
